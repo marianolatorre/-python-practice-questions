2063ed6c8acbcc0e5702cb15d21ce5618b8c26ac
mariano latorre <marianolatorre@gmail.com>
2020-03-25 07:57:32 +0000

    new from leetcode

--- sum-one.py
+++ sum-one.py
@@ -1,16 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        passing = 0
-        for i in len(digits)-1..0:
-            result = digits[i]+1+passing
+        
+        for i in range(len(digits)-1, -1,-1):
+            result = digits[i]+1
             digits[i]= result % 10
             if result > 9:
-                passing = 1
                 if i == 0:
-                    print("no")
                     return [1] + digits
             else:
                 return digits
+        
+        
         
 s=Solution()
 print(s.plusOne([9]))
